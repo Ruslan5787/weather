e9fe77a751622943d4255bef8011fdefe8ea5eec
Ruslan5787 <abdulazimov.03@mail.ru>
2021-12-26 12:46:20 +0500

    revie commit

--- js/main.js
+++ js/main.js
@@ -24,23 +24,24 @@ const MONTHS = [
 
 let citiesArray = []
 
-// ------------------------------------------------
-
+// --------------------------------------------------------------------------------------------
 if (localStorage.length) {
+   const storageArrayCities = JSON.parse(localStorage.getItem('array'))
+   const mainCityName = localStorage.getItem('mainCity')
+   const url = getUrl(mainCityName)
+
    if (localStorage.getItem('array')) {
       citiesArray = JSON.parse(localStorage.getItem('array'))
    }
 
-   const storageArrayCities = JSON.parse(localStorage.getItem('array'))
    for (const key in storageArrayCities) {
       addCity(storageArrayCities[key])
    }
 
-   const mainCityName = localStorage.getItem('mainCity')
    UI.WEATHER.NAME.forEach(element => {
       element.textContent = mainCityName
    })
-   const url = getUrl(mainCityName)
+
    fetch(url)
       .then(response => response.json())
       .then(result => {
@@ -49,14 +50,28 @@ if (localStorage.length) {
       })
 }
 
+function getUrl(value) {
+   return `${REQUEST.WETHER_URL}?q=${value}&appid=${REQUEST.API_KEY}${REQUEST.DEGREES_C}`
+}
+
 function addCity(content) {
    UI.CITIES.LIST.innerHTML += `<div class="weather__right-block">
                                  <div class="weather__right-item">${content}</div>
                                  <button class="weather__right-btn"></button>
                               </div>`
 }
-function getUrl(value) {
-   return `${REQUEST.WETHER_URL}?q=${value}&appid=${REQUEST.API_KEY}${REQUEST.DEGREES_C}`
+
+function getWeatherInfo(result) {
+   UI.WEATHER.DEGREES.textContent = result.main.temp
+   UI.WEATHER.IMG.src = `http://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`
+}
+
+function getWeatherDetails(result) {
+   UI.DETAILS.TEMPERATURE.textContent = result.main.temp
+   UI.DETAILS.FEELS_LIKE.textContent = result.main.feels_like
+   UI.DETAILS.WEATHER.textContent = result.weather[0].main
+   UI.DETAILS.SUNRISE.textContent = result.sys.sunrise
+   UI.DETAILS.SUNSET.textContent = result.sys.sunset
 }
 
 UI.FORM.addEventListener('submit', e => {
@@ -68,39 +83,26 @@ UI.FORM.addEventListener('submit', e => {
       .then(result => {
          if (!(result.cod == 404)) {
             getWeatherInfo(result)
-            changeAllNameCities(value)
+            changeAllCitieNames(value)
+
+            localStorage.setItem('mainCity', value)
          } else {
             throw new Error('Не правильно ввели название города.')
          }
+
          getWeatherDetails(result)
          addWeatherCards(value)
       })
-      .catch(error => {
-         alert(error)
-      })
-   UI.FORM.reset()
-   localStorage.setItem('mainCity', value)
+      .catch(alert)
 
+   UI.FORM.reset()
    e.preventDefault()
 })
 
-function changeAllNameCities(value) {
+function changeAllCitieNames(value) {
    UI.WEATHER.NAME.forEach(e => {
       e.textContent = value
    })
-}
-
-function getWeatherDetails(result) {
-   UI.DETAILS.TEMPERATURE.textContent = result.main.temp
-   UI.DETAILS.FEELS_LIKE.textContent = result.main.feels_like
-   UI.DETAILS.WEATHER.textContent = result.weather[0].main
-   UI.DETAILS.SUNRISE.textContent = result.sys.sunrise
-   UI.DETAILS.SUNSET.textContent = result.sys.sunset
-}
-
-function getWeatherInfo(result) {
-   UI.WEATHER.DEGREES.textContent = result.main.temp
-   UI.WEATHER.IMG.src = `http://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`
 }
 
 UI.CITIES.LIST_ITEMS.forEach(e => {
@@ -118,45 +120,53 @@ function isCityDuplicate(cityName) {
    if (!citiesArray.includes(cityName)) {
       addCity(cityName)
       citiesArray.push(cityName)
+
       localStorage.setItem('array', JSON.stringify(citiesArray))
    }
 }
 
-UI.CITIES.LIST.addEventListener('click', e => {
-   const content = e.target
+UI.CITIES.LIST.addEventListener('click', event => {
+   const content = event.target
    const isNameCity = content.classList.contains('weather__right-item')
    const isBtnCity = content.classList.contains('weather__right-btn')
+
    if (isNameCity) {
-      clickNameCity(e, content)
+      clickNameCity(event, content)
       addWeatherCards(content.textContent)
-      changeAllNameCities(content.textContent)
+      changeAllCitieNames(content.textContent)
    }
+
    if (isBtnCity) {
       clickOnDelBtn(content)
    }
 })
 
 function clickNameCity(event, content) {
-   const url = getUrl(event.target.textContent)
+   const citieName = event.target.textContent
+   const url = getUrl(citieName)
+
    fetch(url)
       .then(response => response.json())
       .then(result => {
          getWeatherInfo(result)
          getWeatherDetails(result)
+
          UI.WEATHER.NAME.textContent = content.textContent
       })
+
    localStorage.setItem('mainCity', content.textContent)
 }
 
 function clickOnDelBtn(content) {
    const weatherBlock = content.closest('.weather__right-block')
    const weatherBlockItem = weatherBlock.querySelector('.weather__right-item').textContent.toLowerCase()
-   const indexCity = citiesArray.indexOf(weatherBlockItem)
-   citiesArray.splice(indexCity, 1)
+   const indexCityOnArray = citiesArray.indexOf(weatherBlockItem)
+
+   citiesArray.splice(indexCityOnArray, 1)
    localStorage.setItem('array', JSON.stringify(citiesArray))
    weatherBlock.remove()
 }
-// ---------------------------------------------------------
+
 UI.FORECAST.TAB_BTN.addEventListener('click', () => {
    const cityName = UI.FORECAST.CITY_NAME.textContent
    addWeatherCards(cityName)
@@ -164,11 +174,14 @@ UI.FORECAST.TAB_BTN.addEventListener('click', () => {
 
 function addWeatherCards(cityName) {
    const url = `${REQUEST.FORECAST_URL}?q=${cityName}&appid=${REQUEST.API_KEY}${REQUEST.DEGREES_C}`
+
    fetch(url)
       .then(response => response.json())
       .then(result => {
-         const resultArr = result.list.splice(0, 3)
+         const resultArr = result.list.splice(0, 14)
+
          UI.CARD.LIST.innerHTML = ''
+
          for (const elem of resultArr) {
             const month = parseInt(elem.dt_txt.substr(0, 7).substr(-2, 2))
             const day = elem.dt_txt.substr(0, 10).substr(-2, 2)
@@ -176,6 +189,7 @@ function addWeatherCards(cityName) {
             const temp = elem.main.temp
             const feels_like = elem.main.feels_like
             const weather = elem.weather[0].main
+
             UI.CARD.LIST.innerHTML += `<div class="weather__tab-card tab-card">
                                        <div class="tab-card__header">
                                           <div class="tab-card__day">${day} ${MONTHS[month - 1]}</div>
@@ -194,5 +208,4 @@ function addWeatherCards(cityName) {
                                     </div>`
          }
       })
-
 }
